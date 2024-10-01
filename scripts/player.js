a19f99f9d6e18d335d30a4456e088fe08fbbd174
Brody Dorris <Dorrisamanda@gmail.com>
2024-10-01 16:20:52 -0600

    Added x and y values

--- scripts/player.js
+++ scripts/player.js
@@ -7,14 +7,94 @@ export class Player {
         this.y = y;
         this.width = 75;
         this.height = 25;
+
+        this.speed = 1;
+
+        this.keyBindings = {
+            up: "ArrowUp",
+            down: "ArrowDown",
+            left: "ArrowLeft",
+            right: "ArrowRight",
+        };
+
+        this.moveing ={
+            up: false,
+            down: false,
+            left: false,
+            right: false,
+        };
+
+        this.wireUpKeyboard();
     }
     
 
-    update() {}
+    wireUpKeyboard() {
+        window.addEventListener("keydown", (e) => {
+            //console.log(e);
+
+
+            switch(e.code) {
+                case this.keyBindings.up:
+                    this.moveing.up = true;
+                    break;
+        
+            }
+        });
+
+
+            window.addEventListener("keyup", (e) => {
+                //console.log(e);
+                switch(e.code) {
+                    case this.keyBindings.up:
+                        this.moveing.up = false;
+                        break;
+                    case this.keyBindings.down:
+                         this.moveing.down = false;
+                        break;
+                        case this.keyBindings.left:
+                            this.moveing.left = false;
+                            break;
+                    case this.keyBindings.right:
+                        this.moveing.right = false;
+                        break;
+            });
+    }
+
+    update() {
+        let dirX = 0;
+        let dirY = 0;
+
+        if(this.moveing.up) {
+            dirY = -1;
+        }
+
+        if(this.moveing.down) {
+            dirY = 1;
+        }
+        
+
+        if(this.moveing.up && this.moveing.down) {
+            dirY = 0;
+        }
+
+        if(this.moveing.left) {
+            dirX = -1;
+        }
+
+        if(this.moveing.right) {
+            dirX = 1;
+        }
+
+        if(this.moveing.right&&this.moveing.left) {
+            dirX = 0;
+        }
+
+        this.x += this.speed * dirX;
+        this.y += this.speed * dirY;
+    }
 
     draw() {
-        ctx.fillStyle = "blue";
-
+        ctx.fillStyle = "turquoise";
         ctx.fillRect(this.x,this.y,this.width,this.height);
     }
 
