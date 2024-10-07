f8abf4fce477126ed3187fe356855e7c31419f92
Brody Dorris <Dorrisamanda@gmail.com>
2024-10-07 16:28:35 -0600

    Tried to make it blink

--- scripts/collectables/goog.js
+++ scripts/collectables/goog.js
@@ -2,32 +2,63 @@
 import { CollectableItem } from "./colectablebase.js";
 
 export class SimpleGoodItem extends CollectableItem {
-    constructor(x=0,y=0) {
-        super(x, y);
+  constructor(x = 0, y = 0) {
+    super(x, y);
 
-        this.width = 25;
-        this.height = 25;
+    this.width = 25;
+    this.height = 25;
 
-        this.despawnTime = 10 * 1000;
-        this.spawnInTime = 5 * 1000;
+    this.despawnTime = 10 * 1000;
+    this.spawnInTime = 5 * 1000;
+    this.despawnWarningTime = 3 * 1000;
 
-        this.lastAlphaTime = 0;
+    this.blink = {
+      interval: 500,
+      lastBlink: 0,
+      isVisible: true,
+    };
+
+    this.lastAlphaTime = 0;
+    this.alpha = 0;
+
+    this.lifeTime = 0;
+
+    this.color = "hsla(112, 100%, 50%, 0%)";
+  }
+
+  update(elapsedTime) {
+if(!this.isColectable) {
+    return;
+}
+
+    this.lifeTime += elapsedTime;
+
+    if (this.lifeTime < this.spawnInTime) {
+      //we hav no full spawned in yet
+      this.alpha = Math.floor((this.lifeTime / this.spawnInTime) * 100);
+    }
+
+    if (this.lifeTime > this.despawnTime - this.despawnWarningTime) {
+      // start blinking
+      //console.log("BLINK");
+      this.blink.lastBlink += elapsedTime;
+      if (this.blink.lastBlink > this.blink.interval) {
+        if (this.blink.isVisible) 
+            console.log("Blink ON");
+          this.alpha = 100;
+        } else {
+          console.log("NO BLINK");
+          this.alpha = 0;
+        }
+        this.blink.lastBlink = 0;
+        this.blink.isVisible = !this.blink.isVisible;
+      }
+
+      if(this.lifeTime > this.despawnTime) {
         this.alpha = 0;
-
-        this.lifeTime = 0;
-
-        this.color = "hsla(112, 100%, 50%, 0%)";
+        this.isColectable = false;
+      }
+      this.color = `hsla(112, 100%, 50%, ${this.alpha}%)`;
     }
+  }
 
-    update(elapsedTime) {
-this.lifeTime += elapsedTime;
-
-if(this.lifeTime < this.spawnInTime) {
-    //we hav no full spawned in yet
-    this.alpha = Math.floor((this.lifeTime / this.spawnInTime) * 100);
-    this.color = `hsla(112, 100%, 50%, ${this.alpha}%)`;
-}
-
-
-    }
-}
